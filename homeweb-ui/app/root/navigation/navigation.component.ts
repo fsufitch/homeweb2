9b6416031ac3e2c7ab0bf2a3b725c96e4e2cc721
Filip Sufitchi <filips@imagemattersllc.com>
2017-10-12 17:38:18 -0400

    Add display for current active section in nav menu

--- homeweb-ui/app/root/navigation/navigation.component.ts
+++ homeweb-ui/app/root/navigation/navigation.component.ts
@@ -5,6 +5,7 @@ import {
   ActionDispatcher,
   NavigationStateService,
   SetSideNavClosedAction,
+  NavigationActiveSection,
 } from 'homeweb-ui/store';
 
 @Component({
@@ -20,6 +21,10 @@ export class NavigationComponent {
     private navigationStateService: NavigationStateService,
   ) {}
   closed$ = this.navigationStateService.getSideNavClosed();
+
+  private activeSection$ = this.navigationStateService.getActiveSection();
+  homeActive$ = this.activeSection$.map(s => s === NavigationActiveSection.Home);
+  aboutMeActive$ = this.activeSection$.map(s => s === NavigationActiveSection.AboutMe);
 
   toggleSideNav() {
     this.closed$.take(1).subscribe(closed => this.dispatcher.dispatch(
