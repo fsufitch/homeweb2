e15a051b927ac41b925b2ac73c86b961d9f1b398
Filip Sufitchi <filips@imagemattersllc.com>
2017-10-12 13:00:03 -0400

    Basic UI files

---
+++ homeweb-ui/vendor-types.d.ts
@@ -0,0 +1,6 @@
+declare module 'popper.js' {
+  class Popper {
+
+  }
+  export { Popper as default }
+}
---
+++ homeweb-ui/vendor.ts
@@ -0,0 +1,40 @@
+///<reference path="./vendor-types.d.ts" />
+
+// import '@angular/core';
+// import '@angular/common';
+// import '@angular/compiler';
+// import '@angular/forms';
+// import '@angular/http';
+// import '@angular/platform-browser';
+// import '@angular/platform-browser-dynamic';
+// import '@angular/router';
+//
+// import '@angular/platform-browser/animations';
+
+// import '@ngrx/core';
+// import '@ngrx/store';
+// import '@ngrx/effects';
+// import '@ngrx/core/add/operator/select';
+// import 'rxjs/Rx';
+// import 'immutable';
+
+import 'moment';
+
+// import './fonts/vendor';
+
+import 'jquery';
+
+// import { default as Popper } from 'popper.js';
+// declare global {
+//   interface Window {
+//     Popper: Popper;
+//   }
+// }
+// window.Popper = Popper;
+
+
+import 'bootstrap';
+require('style-loader!bootstrap/dist/css/bootstrap.min.css');
+
+import 'octicons';
+require('octicons/build/octicons.css');
